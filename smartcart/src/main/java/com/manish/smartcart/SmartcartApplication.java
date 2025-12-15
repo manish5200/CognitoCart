a01d65fb4e3751a02e3bc22ee1370132ae59bf38
manish5200 <manishksingh5200gmail.com>
2025-12-15 20:40:18 +0530

    Initial commit of CognitoCart API project files

---
+++ smartcart/src/main/java/com/manish/smartcart/SmartcartApplication.java
@@ -0,0 +1,15 @@
+package com.manish.smartcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SmartcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcartApplication.class, args);
+	}
+
+}
